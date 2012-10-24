e392dc30d54164e608d3f96fa8c0f43d7490b70d
w9lsky <w9lsky@gmail.com>
2012-10-24 15:23:33 +0800

    11

---
+++ src/main/java/org/fxc/woblog/App.java
@@ -0,0 +1,13 @@
+package org.fxc.woblog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
